3a861436cac1fef87810259996063389fe091943
tahir <user.email>
2024-11-02 16:09:00 +0400

    suallar ve heller

---
+++ HW19/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel.Design;
+
+namespace HW19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region a
+
+
+            //Verilmiş n ve m (n<m) ədədləri arasındakı tək ədədlərin sayını tapın.
+            //int n = 30;
+            //int m = 36;
+            //int count = default;
+            //if (n < m)
+            //{
+            //    for (int i = n; i < m; i++)
+            //    {
+            //        if (i % 2 != 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine(count);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("duzgun ededler daxil edin");
+            //}
+            #endregion
+            #region b
+            //Verilmiş n ve m (n<m) ededleri arasindaki tek ədədlərin cəmini tapin.
+            //int n = 15;
+            //int m = 10;
+            //int sum = default;
+            //if (n < m)
+            //{
+            //    for (int i = n; i <= m; i++)
+            //    {
+            //        if (i % 2 != 0)
+            //        {
+            //            sum += i;
+            //        }
+
+            //    }
+            //    Console.WriteLine(sum);
+            //    
+            //}
+            //else
+            //{
+            //    Console.WriteLine("duzgun edeleri daxil edin");
+            //}
+            #endregion
+            #region c
+            //m -ədədi verilir. 1-dən m-ədədinə kimi olan cüt ədədlərin hasilini tapin.
+            //int m = 6;
+            //int mult = 1;
+            //for(int i=1; i < m; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        mult *= i;
+            //    }
+            //}
+            //Console.WriteLine(mult);
+
+            #endregion
+            #region d
+            //.a - ədədi 3-ə bölünən cüt ədəddirsə "3-e bolunur cutdur" ,3-ə bölünən tək ədəddirsə " ,
+            //3-e bolunur tekdir" əks halda "3-e bolunmur" sözünü ekranda çap edin.
+
+            //int a = 12;
+            //if(a%2==0&&a%3==0)
+            //{
+            //    Console.WriteLine("3-e bolunur cutdur");
+
+            //}
+            //if (a % 3 == 0 && a % 2 != 0)
+            //{
+            //    Console.WriteLine("3-e bolunur tekdir");
+            //}
+            //else if(a%3!=0)
+            //{
+            //    Console.WriteLine("3-e bolunmur");
+            //}
+
+            #endregion
+            #region e
+            //Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+            //int n = 12;
+            //if (n % 2 == 0)
+            //{
+            //    Console.WriteLine("murekkeb ededdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sade ededdir");
+            //}
+            #endregion
+
+        }
+    }
+}
